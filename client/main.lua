6f50023a30ae4d1056098fec058900737195aa27
Dallefar <94932668+Dallefar@users.noreply.github.com>
2024-07-30 16:33:57 +0200

    DrawMarker

--- client/main.lua
+++ client/main.lua
@@ -1,19 +1,26 @@
+local canUse = false
+
 CreateThread(function()
     for k, v in pairs(Config.places) do 
         local point = lib.points.new({ coords = v.coords, distance = 5 })
          
         function point:onEnter()
             local canAccess, textui = lib.callback.await('dalle:identitycheck', false, k)
-            if not canAccess then lib.showTextUI(textui) else lib.showTextUI('Tryk [E] for at åbne ' .. v.name) end
+            if not canAccess then lib.showTextUI(textui) canUse = false else lib.showTextUI('Tryk [E] for at åbne ' .. v.name) canUse = true end
         end
          
         function point:onExit() lib.hideTextUI() end
          
         function point:nearby()
-            local canAccess, textui = lib.callback.await('dalle:identitycheck', false, k)
-            if IsControlJustReleased(0, 38) and canAccess then
-                local input = lib.inputDialog('Change Name', { { type = 'input', label = 'First Name', placeholder = 'Peter', required = true, min = 2, max = 30 }, { type = 'input', label = 'Last Name', placeholder = 'Lars', required = true, min = 2, max = 30 } })
-                if not input then return end lib.callback.await('dalle:identitychange', false, input, k)
+            DrawMarker(2, self.coords.x, self.coords.y, self.coords.z, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 1.0, 1.0, 1.0, 200, 20, 20, 50, false, true, 2, false, nil, nil, false)
+            if canUse and self.currentDistance < 2 then
+                local canAccess, textui = lib.callback.await('dalle:identitycheck', false, k)
+                if IsControlJustReleased(0, 38) then
+                    if canAccess then
+                        local input = lib.inputDialog('Change Name', { { type = 'input', label = 'First Name', placeholder = 'Peter', required = true, min = 2, max = 30 }, { type = 'input', label = 'Last Name', placeholder = 'Lars', required = true, min = 2, max = 30 } })
+                        if not input then return end lib.callback.await('dalle:identitychange', false, input, k)
+                    end
+                end
             end
         end
 
